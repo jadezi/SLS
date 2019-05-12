70cfa2c7f22a13a98b3bf06b9e07f4b7f484c751
jadezi <857827224@qq.com>
2019-05-12 22:37:46 +0800

    布局更新

--- js/my.js
+++ js/my.js
@@ -1,12 +1,6 @@
 function refresh() {
     window.location.reload();
 }
-$(".main .year .list").each(function (e, target) {
-    var $target = $(target),
-        $ul = $target.find("ul");
-    $target.height($ul.outerHeight()), $ul.css("position", "absolute");
-    console.log($ul.outerHeight());
-});
 $(".main .year>h2>a").click(function (e) {
     e.preventDefault();
     $(this).parents(".year").toggleClass("close");
@@ -14,6 +8,5 @@ $(".main .year>h2>a").click(function (e) {
 
 $(".mdui-panel-item").click(function(){
     var num=$(".mdui-panel").find(".mdui-panel-item-open").length;
-    console.log(num);
 });
 $(".main .year .list")
